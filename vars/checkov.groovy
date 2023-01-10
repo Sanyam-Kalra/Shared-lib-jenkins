2649cc9019d67795d8f72a05c4006ab80836f112
Sanyam Kalra <68919514+Sanyam-Kalra@users.noreply.github.com>
2023-01-10 14:00:33 +0530

    Update checkov.groovy

--- vars/checkov.groovy
+++ vars/checkov.groovy
@@ -2,5 +2,5 @@ def call (Map stepParams){
      echo 'Scaning Infrastructure git   code'
      code_dir="${stepParams.code_dir}"
      echo "${code_dir}"
-     sh 'checkov -f "${code_dir}"'
+     sh 'checkov -f ${code_dir}'
 }
