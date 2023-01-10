1fed32f2eb2006dbbf8c1169c41764d0c6ce09af
Sanyam Kalra <68919514+Sanyam-Kalra@users.noreply.github.com>
2023-01-10 14:15:08 +0530

    Update checkov.groovy

--- vars/checkov.groovy
+++ vars/checkov.groovy
@@ -1,6 +1,11 @@
 def call (Map stepParams){
      echo 'Scaning Infrastructure git   code'
      code_dir="${stepParams.code_dir}"
-     echo "${code_dir}"
-     sh "checkov -f ${code_dir}"
+     echo "${code_dir}" 
+    if ( stepParams.code_dir == "*.tf" ) {
+    sh "checkov -f ${code_dir}"
+  }
+  else{ 
+     sh "checkov -d ${code_dir}"
+  }
 }
