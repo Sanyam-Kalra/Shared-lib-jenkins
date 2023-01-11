7e0433d6d12f40ba63e25bf3824c9d961a8430f0
Sanyam Kalra <68919514+Sanyam-Kalra@users.noreply.github.com>
2023-01-11 12:40:44 +0530

    Update checkov.groovy

--- vars/checkov.groovy
+++ vars/checkov.groovy
@@ -2,7 +2,7 @@ def call (Map stepParams){
      echo 'Scaning Infrastructure git   code'
      code_dir="${stepParams.code_dir}"
      echo "${code_dir}" 
-     if ( "${code_dir}" == "ec2.tf" )
+     if ("${code_dir}" ==~ *.tf)    // if (item.name ==~ /AAA.*/){  
      {
      sh "checkov -f ${code_dir}"
      }
